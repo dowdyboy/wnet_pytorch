8de26a429a3f48eba28fd060659dcb409e76072d
dowdyboy <1203550038@qq.com>
2021-11-01 14:56:36 +0800

    创建组件接口：基础卷积

---
+++ wnet/backbone/component/basic_conv.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+
+class BasicConv(nn.Module):
+    
+    def __init__(self):
+        super(BasicConv, self).__init__()
+
+    def forward(self):
+        pass
