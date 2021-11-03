5fe84629d951f52c53e71e039068baee198f15da
dowdyboy <1203550038@qq.com>
2021-11-03 14:54:23 +0800

    提交basic conv模块

--- wnet/backbone/component/basic_conv.py
+++ wnet/backbone/component/basic_conv.py
@@ -6,6 +6,12 @@ class BasicConv(nn.Module):
     
     def __init__(self, in_channel, out_channel, k, p, s):
         super(BasicConv, self).__init__()
+        self.conv = nn.Conv2d(in_channel, out_channel, k, s, p)
+        self.bn = nn.BatchNorm2d(out_channel)
+        self.act = nn.ReLU()
 
     def forward(self, x):
-        pass
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.act(x)
+        return x
