79bb738fbd118601052dfd0b149f3bfddc9055d8
Jingbo Gao <jingbo_gao@163.com>
2021-11-09 16:05:47 +0800

    更改文件

--- wnet/backbone/component/d_fuse_block.py
+++ wnet/backbone/component/d_fuse_block.py
@@ -7,13 +7,14 @@ class DFuseBlock(nn.Module):
 
     def __init__(self, channel):
         super(DFuseBlock, self).__init__()
-        self.conv311_1to1 = BasicConv(channel, channel, 3, 1, 1)
+        self.conv311_1to1_1 = BasicConv(channel, channel, 3, 1, 1)
+        self.conv311_1to1_2 = BasicConv(channel, channel, 3, 1, 1)
         self.conv311_2to1 = BasicConv(2 * channel, 2 * channel, 3, 1, 1)
         self.conv101 = BasicConv(2*channel, channel, 1, 0, 1)
 
     def forward(self, x1, y1):
-        x2 = self.conv311_1to1(x1)
-        y2 = self.conv311_1to1(y1)
+        x2 = self.conv311_1to1_1(x1)
+        y2 = self.conv311_1to1_2(y1)
         z1 = torch.cat((x2, y2), dim=1)
         z2 = self.conv311_2to1(z1)
         z3 = self.conv101(z2)
